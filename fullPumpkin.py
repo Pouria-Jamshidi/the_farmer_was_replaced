46be5f84db40af48ab3193176325f02a3ffcd882
Pouria-Jamshidi <the.eye.programming@gmail.com>
2025-12-28 18:52:08 +0330

    changed pumpkin function to a custom world size one

--- fullPumpkin.py
+++ fullPumpkin.py
@@ -1,53 +1,49 @@
 import movements
 from neededFuctions import watering
 
-limit = get_world_size()
-
-
-# planting the pumpkins (the watered input is a float number for our moisture level)
-def pumpkinFarm(watered=False, fertilizing=False):
-	# move to the start
-	movements.goTo()
+def customSized_singleDrone_pumpkinFarm(world_size, watered = False, fertelized = False):
+	# """
+	# Create a custom size world for farming only pumpkin with a single drone\n
+	
+	# :param world_size: creates a n*n world. any number less than 3 means full world size
+	# :param watered: should the drone water, and if yes gow dry the ground should be to water (between 0 and 1)
+	# :param fertelized: should we add fertilizer or no (0/False = no , 1/True = yes)
+	# """
+	set_world_size(world_size)
+	limit = get_world_size()
 	for i in range(limit):
+
 		for j in range(limit):
-			# tilling the ground if not tilled
-			if get_ground_type() == Grounds.Grassland:
+			if get_ground_type() != Grounds.Soil:
 				till()
 			plant(Entities.Pumpkin)
-			# watering or fertilizing depending on our input (defult = no water and fertilizer)
 			if watered:
 				watering(watered)
-			if fertilizing:
+			if fertelized:
 				use_item(Items.Fertilizer)
 			move(East)
+
+		all_ripe = False
+		while not all_ripe:
+			all_ripe = True
+			for j in range(limit):
+				if get_entity_type() == Entities.Dead_Pumpkin:
+						all_ripe = False
+						all_ripe = False
+						plant(Entities.Pumpkin)
+						if fertelized:
+							use_item(Items.Fertilizer)
+						if watered:
+							watering(watered)
+
+				move(East)
 		move(North)
-
-
-# start farming the land for pumpkin
-def startFarmingPumpkin(watered=False, fertilizing=False):
-	for i in range(limit):
-		for j in range(limit):
-			if get_entity_type() == Entities.Dead_Pumpkin:
-				if fertilizing:
-					#due to a bug or coding error, if fertilizer is used , it changes the state of pumpking.
-					#dead ones stay dead , but if normal, it starts to grow healthy again.
-					#this while loop ensures enough fertilizer used to recreate a healty pumpkin (current ver. 1.0 , workes till patched)
-					while get_entity_type()==Entities.Dead_Pumpkin:
-						use_item(Items.Fertilizer)
-				else:
-					plant(Entities.Pumpkin)
-			harvest()
-			plant(Entities.Pumpkin)
-			# watering or fertilizing depending on our input (defult = no water and fertilizer)
-			if watered:
-				watering(watered)
-			if fertilizing:
-				use_item(Items.Fertilizer)
-			move(East)
-		move(North)
-
+	harvest()
 
 if __name__ == "__main__":
-	pumpkinFarm(0,1)
+	# pumpkinFarm(0,1)
+	# while True:
+	# 	startFarmingPumpkin(0,1)
+
 	while True:
-		startFarmingPumpkin(0,1)
+		customSized_singleDrone_pumpkinFarm(6,1,1)
